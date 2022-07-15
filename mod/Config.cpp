b0c057279f4078972050bbe34f6cc498b095c128
Igor Koveshnikov <igormafiozy@gmail.com>
2022-07-15 20:06:43 +0300

    v1.0.0

---
+++ mod/Config.cpp
@@ -0,0 +1,208 @@
+class CfgPatches {
+
+	class igoryanych_addon
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		version = "1.0.0";
+		author[]= {"Igoryanych_"};
+		authorUrl = "https://github.com/Igoryanych-WT";
+	};
+
+};
+
+	/*=================================================================
+	
+	CREATOR
+
+	-> https://steamcommunity.com/id/igormafiozy
+	
+	--------------------------------------------------------------------
+	-> If you need any help with this, go here: https://steamcommunity.com/groups/arma3insignias
+		and open a new discussion. I will then try to help you!
+	
+	=================================================================*/
+
+class CfgUnitInsignia
+{
+	
+	class vks_polev2
+	{
+		displayName = "ВКС полевой";
+		author = "Igoryanych_";
+		texture = "\36097\icon\vks_pole.paa";
+		textureVehicle = "";
+	};
+	
+	class vks_vishitav2
+	{
+		displayName = "ВКС вышит.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\vks.paa";
+		textureVehicle = "";
+	};
+	
+	/*=================================================================
+	class vks_pole
+	{
+		displayName = "ВКС КДА полевой";
+		author = "Igoryanych_";
+		texture = "\36097\icon\36097p.paa";
+		textureVehicle = "";
+	};
+	
+	class vks_vishita
+	{
+		displayName = "ВКС КДА вышит.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\36097.paa";
+		textureVehicle = "";
+	};
+	=================================================================*/
+	
+	class renegat_orig
+	{
+		displayName = "Нашивка ренегатов ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\renegat_orig.paa";
+		textureVehicle = "";
+	};
+	
+	class bandit_orig
+	{
+		displayName = "Нашивка бандитов ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\bandit_orig.paa";
+		textureVehicle = "";
+	};
+	
+	class clear_sky_orig
+	{
+		displayName = "Нашивка чистового неба ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\clear_sky_orig.paa";
+		textureVehicle = "";
+	};
+	
+	class stalker_orig
+	{
+		displayName = "Нашивка сталкеров ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\stalker_orig.paa";
+		textureVehicle = "";
+	};
+	
+	class military_orig
+	{
+		displayName = "Нашивка военных ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\military_orig.paa";
+		textureVehicle = "";
+	};
+	
+	class greh_orig
+	{
+		displayName = "Нашивка грех ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\greh_orig.paa";
+		textureVehicle = "";
+	};
+	
+	class last_day_l
+	{
+		displayName = "Нашивка последний день(Л) ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\last_day_l.paa";
+		textureVehicle = "";
+	};
+	
+	class last_day_r
+	{
+		displayName = "Нашивка последний день(Р) ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\last_day_r.paa";
+		textureVehicle = "";
+	};
+	
+	class duty_orig
+	{
+		displayName = "Нашивка долг ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\duty_orig.paa";
+		textureVehicle = "";
+	};
+	
+	class duty_a
+	{
+		displayName = "Нашивка долг альтер.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\duty_a.paa";
+		textureVehicle = "";
+	};
+	
+	class duty_a_pole
+	{
+		displayName = "Нашивка долг альтер. полевая";
+		author = "Igoryanych_";
+		texture = "\36097\icon\duty_a_pole.paa";
+		textureVehicle = "";
+	};
+	
+	class freedom_orig_l
+	{
+		displayName = "Нашивка свобода(Л) ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\freedom_orig_l.paa";
+		textureVehicle = "";
+	};
+	
+	class freedom_orig_r
+	{
+		displayName = "Нашивка свобода(Р) ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\freedom_orig_r.paa";
+		textureVehicle = "";
+	};
+	
+	class ecolog_orig
+	{
+		displayName = "Нашивка экологов ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\ecolog_orig.paa";
+		textureVehicle = "";
+	};
+	
+	class merc_orig_l
+	{
+		displayName = "Нашивка наёмников(Л) ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\merc_orig_l.paa";
+		textureVehicle = "";
+	};
+	
+	class merc_orig_r
+	{
+		displayName = "Нашивка наёмников(Р) ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\merc_orig_r.paa";
+		textureVehicle = "";
+	};
+	
+	class monolith_orig_l
+	{
+		displayName = "Нашивка монолит(Л) ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\monolith_orig_l.paa";
+		textureVehicle = "";
+	};
+	
+	class monolith_orig_r
+	{
+		displayName = "Нашивка монолит(Р) ориг.";
+		author = "Igoryanych_";
+		texture = "\36097\icon\monolith_orig_r.paa";
+		textureVehicle = "";
+	};
+	
+};
